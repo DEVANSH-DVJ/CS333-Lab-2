c00f74b09f902ca8fdf4ede404e3f436b30e3414
devansh-dvj <devansh.dvj@gmail.com>
2021-09-03 15:26:48 +0530

    A: Tested getMaxPid(void)

--- xv6-public/tc-procinfo.c
+++ xv6-public/tc-procinfo.c
@@ -7,7 +7,7 @@ int main(void) {
   // struct processInfo info;
   // int pid;
   printf(1, "Total Number of Active Processes: %d\n", getNumProc());
-  // printf(1, "Maximum PID: %d\n\n", getMaxPid());
+  printf(1, "Maximum PID: %d\n\n", getMaxPid());
 
   // printf(1, "PID\tPPID\tSIZE\tNumber of Context Switch\n");
   // for (int i = 1; i <= getMaxPid(); i++) {
