518574f39c68e82f46acffcaa8c354502ae373f9
devansh-dvj <devansh.dvj@gmail.com>
2021-09-03 15:55:15 +0530

    A: Tested base of syscall getProcInfo

--- xv6-public/tc-procinfo.c
+++ xv6-public/tc-procinfo.c
@@ -4,17 +4,19 @@
 #include "processInfo.h"
 
 int main(void) {
-  // struct processInfo info;
-  // int pid;
+  struct processInfo info;
+  int pid;
+
   printf(1, "Total Number of Active Processes: %d\n", getNumProc());
   printf(1, "Maximum PID: %d\n\n", getMaxPid());
 
-  // printf(1, "PID\tPPID\tSIZE\tNumber of Context Switch\n");
-  // for (int i = 1; i <= getMaxPid(); i++) {
-  //   pid = i;
-  //   if (getProcInfo(pid, &info) == 0)
-  //     printf(1, "%d\t%d\t%d\t%d\n", pid, info.ppid, info.psize,
-  //            info.numberContextSwitches);
-  // }
+  printf(1, "PID\tPPID\tSIZE\tNumber of Context Switch\n");
+  for (int i = 1; i <= getMaxPid(); i++) {
+    pid = i;
+    if (getProcInfo(pid, &info) == 0)
+      printf(1, "%d\t%d\t%d\t%d\n", pid, info.ppid, info.psize,
+             info.numberContextSwitches);
+  }
+
   exit();
 }
